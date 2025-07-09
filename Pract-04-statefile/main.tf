574a88528c3cd3eb5f6aec9db05932d60064660e
Hariom Kabra <143120733+Hariomkabra@users.noreply.github.com>
2025-07-09 22:04:00 +0530

    updated

---
+++ Pract-04-statefile/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "name" {
+    ami = "ami-05ffe3c48a9991133"  // Replace with a valid AMI ID
+    instance_type = "t2.micro"
+    key_name = "test"
+
+    tags = {
+        day = "day - 04"
+    }
+  
+}
